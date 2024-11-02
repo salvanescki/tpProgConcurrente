4f233ff594965f52abaf063b56d28a533d60e812
Nicolás-Salvatore <30707653+Unknow-Error@users.noreply.github.com>
2024-11-02 08:07:09 -0300

    Update Worker.java
    Que cada Worker modifiquen el contador de Workers activos o lo bajen si ya usaron la PoisonPill

--- src/main/java/org/example/Worker.java
+++ src/main/java/org/example/Worker.java
@@ -3,6 +3,7 @@ package org.example;
 public class Worker extends Thread {
 
     private final Buffer buffer;
+    private final WorkerCounter workerCounter;
 
     public Worker(Buffer buffer) {
         this.buffer = buffer;
@@ -13,11 +14,13 @@ public class Worker extends Thread {
         try {
             while (true) {
                 // Acá iría un deserialize() si hubiera que ponerlo (o un cast a Runnable).
+                workerCounter.incrementar();
                 Runnable task = buffer.read();
                 task.run();
             }
-        } catch (Exception e) {
+        } catch (PoisonException e) {
             Thread.currentThread().interrupt();
         }
+        workerCounter.decrementar();
     }
 }
