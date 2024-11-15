54baead76c41c6ee8f842fe0347fb6d34efecc6f
Nicolás-Salvatore <30707653+Unknow-Error@users.noreply.github.com>
2024-11-15 19:05:40 -0300

    Create ResultadoGlobal.java

---
+++ src/main/java/org/example/ResultadoGlobal.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class ResultadoGlobal {
+	private final PriorityQueue<KNearestNeighbors.PuntoDistancia> globalKVecinos;
+
+    public ResultadoGlobal(int k) {
+        this.globalKVecinos = new PriorityQueue<>(Comparator.comparingLong(p -> p.distancia));
+    }
+
+    public synchronized void agregarVecinos(PriorityQueue<KNearestNeighbors.PuntoDistancia> vecinosLocales) {
+        while (!vecinosLocales.isEmpty()) {
+            globalKVecinos.add(vecinosLocales.poll());
+        }
+    }
+
+    public synchronized PriorityQueue<KNearestNeighbors.PuntoDistancia> obtenerResultados() {
+        return new PriorityQueue<>(globalKVecinos); // Copia para evitar modificar el original
+    }
+}
