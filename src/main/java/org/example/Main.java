7c75810bbcbb4a3bb5cdb5e3ff93df88b70d6a3d
Nicolás-Salvatore <30707653+Unknow-Error@users.noreply.github.com>
2024-11-15 19:08:59 -0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,48 +3,48 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         /*
-		 * El args[] es para los parámetros siguientes:
-		 * -modo : imagen simple con los pixeles solicitados o el archivo de test (.csv)
-		 * -k : cantidad de k vecinos más cercanos o k imágenes más parecidas, deseados.
-		 * -cant_threads : cantidad de Threads workers que va a crear el ThreadPool.
-		 * -tamano_buffer : El tamaño del buffer.
-		 */
+	* El args[] es para los parámetros siguientes:
+	* -modo : imagen simple con los pixeles solicitados o el archivo de test (.csv)
+	* -k : cantidad de k vecinos más cercanos o k imágenes más parecidas, deseados.
+	* -cant_threads : cantidad de Threads workers que va a crear el ThreadPool.
+	* -tamano_buffer : El tamaño del buffer.
+	*/
 		
-		int kNeighbors = Integer.parseInt(args[0]); //args[1];
-		int numWorkers = Integer.parseInt(args[1]); //args[2];
-		int bufferSize = Integer.parseInt(args[2]); //args[3];
-		String modo = args[3]; //"imagen" o "CSV"
-		String archivoPath = args[4]; //"mnist_test.csv" o "prueba.png"
-		String dataSetEntrenamientoPath = args[5]; //"mnist_train.csv"
+	int kNeighbors = Integer.parseInt(args[0]); //args[1];
+	int numWorkers = Integer.parseInt(args[1]); //args[2];
+	int bufferSize = Integer.parseInt(args[2]); //args[3];
+	String modo = args[3]; //"imagen" o "CSV"
+	String archivoPath = args[4]; //"mnist_test.csv" o "prueba.png"
+	String dataSetEntrenamientoPath = args[5]; //"mnist_train.csv"
 		
-		long tiempoInicio = System.currentTimeMillis();
-		WorkerCounter workerCounter = new WorkerCounter();
-		ThreadPool pool = new ThreadPool(bufferSize, numWorkers, workerCounter);
-		ResultadoGlobal resultadoGlobal = new ResultadoGlobal(kNeighbors);
+	long tiempoInicio = System.currentTimeMillis();
+	WorkerCounter workerCounter = new WorkerCounter();
+	ThreadPool pool = new ThreadPool(bufferSize, numWorkers, workerCounter);
+	ResultadoGlobal resultadoGlobal = new ResultadoGlobal(kNeighbors);
 		
-		// Cargar conjunto de entrenamiento
-                List<Image> datasetEntrenamiento = cargarDataset(dataSetEntrenamientoPath, 0, Integer.MAX_VALUE);
+	// Cargar conjunto de entrenamiento
+        List<Image> datasetEntrenamiento = cargarDataset(dataSetEntrenamientoPath, 0, Integer.MAX_VALUE);
 		
-		//Cargar tareas según el modo
-		if(modo.equals("imagen")) {
-			//Crear una tarea para una imagen
-			analizarImagen(pool, archivoPath, datasetEntrenamiento, kNeighbors);
-		} else if (modo.equals("CSV")) {
-			analizarCSV(pool, archivoPath, datasetEntrenamiento, kNeighbors);
-		}
+	//Cargar tareas según el modo
+	if(modo.equals("imagen")) {
+	         //Crear una tarea para una imagen
+		analizarImagen(pool, archivoPath, datasetEntrenamiento, kNeighbors);
+	} else if (modo.equals("CSV")) {
+		analizarCSV(pool, archivoPath, datasetEntrenamiento, kNeighbors);
+	}
 		
-		/*
-		for(int i = 1; i <= 100; i++) {
-			pool.iniciar(new DummyTask("Task No." + i + "."));
-		}
-		*/
+	/*
+	for(int i = 1; i <= 100; i++) {
+		pool.iniciar(new DummyTask("Task No." + i + "."));
+	}
+	*/
 		
-		pool.detener();
-		workerCounter.trabajoTerminado(); //Espera a que no hayan trabajadores activos.
+	pool.detener();
+	workerCounter.trabajoTerminado(); //Espera a que no hayan trabajadores activos.
 		
-		long tiempoFinal = System.currentTimeMillis();
-		System.out.println("El tiempo total de ejecución fue de : " + (tiempoFinal-tiempoInicio) + " milisegundos.");
-		System.out.println("Todos los threads Workers han terminado sus tareas");
+	long tiempoFinal = System.currentTimeMillis();
+	System.out.println("El tiempo total de ejecución fue de : " + (tiempoFinal-tiempoInicio) + " milisegundos.");
+	System.out.println("Todos los threads Workers han terminado sus tareas");
     }
 
     private static void analizarImagen(ThreadPool threadPool, String filePath, List<Image> datasetEntrenamiento, int k) {
