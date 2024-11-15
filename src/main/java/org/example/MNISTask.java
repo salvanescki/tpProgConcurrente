125c25892e6a8689898ca90e5883c40e23e05257
Nicolás-Salvatore <30707653+Unknow-Error@users.noreply.github.com>
2024-11-15 19:04:48 -0300

    Update MNISTask.java

--- src/main/java/org/example/MNISTask.java
+++ src/main/java/org/example/MNISTask.java
@@ -1,10 +1,36 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class MNISTask extends Task {
 
-    @Override
-    public void run() {
-
-    }
+    private final int k;
+	private final Image imagenTarget;
+	private final ResultadoGlobal resultadoGlobal;
+	private final List<Image> dataSet;
+	
+	public MNISTask(Image imagenTarget, List<Image> dataSet, int k, ResultadoGlobal resultadoGlobal) {
+		this.imagenTarget = imagenTarget;
+        this.dataSet = dataSet;
+        this.k = k;
+        this.resultadoGlobal = resultadoGlobal;
+	}
+	
+	@Override
+	public void run() {
+		List<byte[]> dataSetBytes = new ArrayList<>(); 
+		for (Image image : dataSet) {
+		    dataSetBytes.add(image.getColors()); 
+		}
+		
+		PriorityQueue<KNearestNeighbors.PuntoDistancia> kVecinos =
+				KNearestNeighbors.obtenerKVecinosMasCercanos(imagenTarget.getColors(), dataSetBytes, k);
+		
+		// Registrar los resultados en la estructura global
+        synchronized (resultadoGlobal) {
+            resultadoGlobal.agregarVecinos(kVecinos);
+        }
+	}
 
 }
